5e1b831762f080e2d7d4a03f22c66e45e8eb4071
ubuntu <ubuntu@localhost.localdomain>
2019-07-28 03:01:58 +0800

    add gitignore item

---
+++ bingoai_server/config.py
@@ -0,0 +1,9 @@
+import os
+
+certifile = '/home/ubuntu/bingo_ai/bingoai_server/ssl/nginx/bingoai.club.crt'
+keyfile = '/home/ubuntu/bingo_ai/bingoai_server/ssl/nginx/bingoai.club.key'
+
+loglevel = 'debug'
+bind = "0.0.0.0:5000"
+
+workers = 3
